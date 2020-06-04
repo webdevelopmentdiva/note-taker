e73f81f36555a1c449c7e5238abdadc51008e47a
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-04 15:37:22 -0700

    Serve, ran dependencies, set up express app, listen for anything on port

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,37 @@
+//Dependencies
+//======================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+//Sets up the express app
+// ==========================
+var app = express();
+var PORT = 8080;
+
+//Sets up the express app to handle the data parsing
+//======================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES
+//========
+
+//HTML Routes
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, " notes.html"));
+});
+
+// API Routes
+app.get("/api/notes", function (req, res) {
+  res.json(db);
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("APP listening on PORT " + PORT);
+});
