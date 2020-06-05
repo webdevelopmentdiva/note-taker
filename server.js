393415095beac3384a34e4dd7bf342e787f94210
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-05 13:59:34 -0700

    fixed bug

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const fs = require("fs");
 
 //Sets up the express app
 // ==========================
-var app = express();
-var PORT = 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// const publicPath = path.resolve(__dirname, "public");
+// const dbPath = path.resolve(__dirname, "db");
 
 //Sets up the express app to handle the data parsing
 //======================================================
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(publicPath));
 app.use(express.json());
 
 //ROUTES
@@ -23,8 +27,15 @@ app.get("/", function (req, res) {
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "notes.html"));
 });
+app.get("/styles.css", function (req, res) {
+  res.sendFile(path.join(__dirname, "styles.css"));
+});
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
 
 // API Routes
 app.get("/api/notes", function (req, res) {
