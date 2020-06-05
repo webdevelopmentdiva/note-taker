97fb2c881b17a3c77bb838ebe573c12612067010
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-04 17:06:50 -0700

    having an issue with the paths

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, " notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // API Routes
